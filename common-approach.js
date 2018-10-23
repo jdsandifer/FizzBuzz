75cca68e2498560f4c0ff00267a8c81a9d4bdb73
J.D. Sandifer <sandifer.jd@gmail.com>
2018-10-23 11:42:03 -0700

    Create common-approach.js

---
+++ common-approach.js
@@ -0,0 +1,17 @@
+// Common approach using a loop and conditionals
+// This one assumes uses modulus 15 as a shortcut for
+// checking for factors of 3 and 5 at the same time.
+
+const fizzBuzz = (max) => {
+  for (let i = 1; i <= max; i++) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz")
+    } else if (i % 3 === 0) {
+      console.log("fizz")
+    } else if (i % 5 === 0) {
+      console.log("buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
