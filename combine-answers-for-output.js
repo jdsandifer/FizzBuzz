74a40205bcec8ae664ec93a0144eaae504a3e7dd
J.D. Sandifer <sandifer.jd@gmail.com>
2018-12-17 22:02:42 -0800

    Create combine-answers-for-output.js

---
+++ combine-answers-for-output.js
@@ -0,0 +1,29 @@
+// Common approach using loop and conditionals
+// except no output is generated until the end.
+// This approach to problems can be useful where
+// lots of data needs to be output, written to a file,
+// or transmitted across a network. One large burst
+// of data can often be faster than many smaller
+// transmissions of data spread out over time. 
+
+const fizzBuzz = (max) => { 
+  const lineTerminator = "\n"
+  let output = ""
+  
+  for (let i = 1; i <= max; i++) {
+    const isDivisibleByThree = i % 3 === 0
+    const isDivisibleByFive = i % 5 === 0
+
+    if (isDivisibleByThree && isDivisibleByFive) {
+      output += "fizzbuzz" + lineTerminator
+    } else if (isDivisibleByThree) {
+      output += "fizz" + lineTerminator
+    } else if (isDivisibleByFive) {
+      output += "buzz" + lineTerminator
+    } else {
+      output += i + lineTerminator
+    }
+  }
+
+  console.log(output)
+}
