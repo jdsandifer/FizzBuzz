b91ef0af570db979d97328825b2be9355699f8d0
J.D. Sandifer <sandifer.jd@gmail.com>
2018-12-20 11:27:08 -0800

    Add parameter defaults

--- options-for-fizz-and-buzz-divisors.js
+++ options-for-fizz-and-buzz-divisors.js
@@ -3,12 +3,14 @@
 // However, I've modified it to allow for a choice of
 // numbers to be used as the divisors. This is an example
 // of providing flexibility for future (or imminent) modification.
+// Providing default parameters prevents code that still uses
+// the simpler message signature from breaking with this change.
 // It's fun to play with different divisors and notice
 // the patterns of fizz's and buzz's produced. 
 // Sometimes it's easy to see them, and sometimes it's 
 // hard to pick out the pattern. Try it for yourself!
 
-const fizzBuzz = (max, fizzDivisor, buzzDivisor) => {
+const fizzBuzz = (max, fizzDivisor = 3, buzzDivisor = 5) => {
   let output = ""
   const lineTerminator = "\n"
   
