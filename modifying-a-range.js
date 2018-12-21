d8102afcbb02c455b9b8ce1d0464cf037396fb20
J.D. Sandifer <sandifer.jd@gmail.com>
2018-12-21 09:16:21 -0800

    Create modifying-a-range.js

---
+++ modifying-a-range.js
@@ -0,0 +1,30 @@
+// This approach starts with a list of all of 
+// the numbers to be considered and then alters
+// the list by replacing the numbers with the 
+// words where appropriate before outputting 
+// the lot.
+
+const fizzBuzz = max => {
+  let numbers = range(max)
+  numbers = numbers.map(num => {
+    const divisibleByThree = num % 3 === 0
+    const divisibleByFive = num % 5 === 0
+
+    if (divisibleByThree && divisibleByFive) {
+      return "fizzbuzz"
+    } else if (divisibleByThree) {
+      return "fizz"
+    } else if (divisibleByFive) {
+      return "buzz"
+    } else {
+      return num
+    }
+  })
+  const output = numbers.join("\n")
+  console.log(output)
+}
+
+const range = max => {
+  const indexPlusOne = (_, i) => i + 1
+  return Array.apply(null, {length: max}).map(indexPlusOne)
+}
